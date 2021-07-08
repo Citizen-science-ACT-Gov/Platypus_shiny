1b6a60d419fa500960459406c0366c603eaaacd5
Citizen-science-ACT-Gov <rodney.ubrihien@act.gov.au>
2021-07-08 04:53:52 +0000

    update

--- Shiny_plat.R
+++ Shiny_plat.R
@@ -12,19 +12,19 @@
 
 #1 load libraries 
 
-install.packages("dplyr")
+#install.packages("dplyr")
 
-install.packages("tidyr")
+#install.packages("tidyr")
 
-install.packages("lubridate")
+#install.packages("lubridate")
 
-install.packages("ggplot2")
+#install.packages("ggplot2")
 
-install.packages("shiny")
+#install.packages("shiny")
 
-install.packages("leaflet")
+#install.packages("leaflet")
 
-install.packages("plotly")
+#install.packages("plotly")
 
 
 library(dplyr)
@@ -41,114 +41,175 @@ library(leaflet)
 
 library(plotly)
 
-#2 Read in platypus month data 
+#2 Read and tidy platypus month data 
 
 p <- read.csv("app_data.csv")
 
-str(p)
 
 p <- p %>% mutate(
-  longitude = as.numeric(longitude),
-  date = as.Date(date, format = "%d/%m/%Y")
+   date = as.Date(date, format = "%d/%m/%Y"),
+   species = trimws(species)
 )
+
+
+cleanup <- theme(panel.grid.major = element_blank(),
+                 panel.grid.minor = element_blank(),
+                 panel.background = element_blank(),
+                 axis.line = element_line(color = "black"),
+                 panel.grid.major.y = element_line(colour = "gray", linetype = "dotted", size = 0.1),
+                 axis.text.x = element_text(color = "black"),
+                 axis.text.y = element_text(color = "black"))
+
+platyIcons <- iconList(
+   "survey" = makeIcon("platy_image.png", 40, 40),
+   "adhoc" = makeIcon("platy_image_2.png", 40, 40)
+)
+
+
 
 
 #3 set up a ui for the shiny app
 
-ui <- fluidPage(titlePanel("Platypus observations - ACT waterwatch"),
-        sidebarLayout(
-          sidebarPanel(
-            
-            #select species 
-            selectInput(inputId = "species", label = "Species",
-                        choices = unique(p$species),
-                        selected = "platypus"),
-            
-            
-            
-            #select data type
-            selectInput(inputId = "type", label = strong("Type of data"),
-                        choices = unique(p$type),
-                        selected = "survey"),
-            
-            # select site
-            selectInput(inputId = "site", label = "Site",
-                        choices = unique(p$site)),
-            selected = "Cooma_Ck"),
-          
-          # select the time period for the output
-          dateRangeInput(inputId = "date", 
-                        label = "Date range",
-                        format = "dd/mm/yyyy",
-                        separator = "to"),
-          
-          # Add leaflet map
-          leafletOutput("my_map")),
-          
-          #Output figure and table
-        
-        mainPanel
-        (plotlyOutput(outputId = "barplot", height = "400px"),
-          ))
+ui <- fluidPage(
+   # App title 
+   titlePanel("Platypus and Rakali observations - ACT Waterwatch"),
+   
+   sidebarLayout(
+      sidebarPanel(
+   
+   #select species 
+   selectInput(inputId = "species", label = "Species",
+               choices = unique(p$species),
+               selected = "platypus"),
+   
+   #select data type
+   selectInput(inputId = "type", label = strong("Type of data"),
+               choices = unique(p$Type),
+               selected = "survey"),
+   
+   # select site
+   selectInput(inputId = "site", label = "Site",
+               choices = unique(p$site_id),
+   selected = "Cooma_Ck"),
 
+   # select the time period for the output
+  dateRangeInput(inputId = "date", 
+               label = "Date range",
+               format = "dd/mm/yyyy",
+               start = "2016-01-01",
+               separator = "to"),
+  
+  # Add leaflet map
+  leafletOutput("my_map")),
+   
+  
+  #add main panel
+  mainPanel(
+            plotOutput(outputId = "col_plot", height = "400px"),
+  
+  
+  dataTableOutput(outputId = "survey_table"),
+  "Testing to see if text is added",
+  
+  # testing to add a picture to the APP
+  tags$img(height = 100, width = 300,
+           src = "https://github.com/Citizen-science-ACT-Gov/Platypus_shiny/blob/main/Waterwatch_logo_Upper_Murrumbidgee.png")
+  
+  )
 
+))
 
-
+       
 
 #4 Define server function 
+
+html_legend <- "<img src='https://github.com/Citizen-science-ACT-Gov/platy_images/blob/main/platy_image.png'>survey<br/>
+<img src='https://github.com/Citizen-science-ACT-Gov/platy_images/blob/main/platy_image_2.png'>Adhoc"
+
+
+
+
+
           
- server <-  function (input, output, session) {
-   
-   #subset data by species
-   selected_species = reactive ({
-      p %>%
-     filter(species == input$species )
+ server <- function (input, output) {
+   output$my_map <- renderLeaflet({
+      
+      leaflet(p) %>%
+         addTiles() %>%
+         fitBounds(lng1 = 149.0, lat1 = -35.0, lng2 = 150.0, lat2 = -36.0)%>%
+         addMarkers(~longitude, ~latitude, icon = platyIcons[p$Type]) 
+      
    })
    
-   # select the type of data to be used
-   selected_data = reactive ({
+   # subset data for plot 
+   
+   plot_data <- reactive({
+            p %>%
+               filter(
+                  species == input$species &
+                  Type == input$type &
+                  site_id == input$site &
+                  date >=  input$date[1] &
+                     date <= input$date[2])
+               })
+   
+ # Create a reactive plot based on the inputs listed
+   
+  output$col_plot <- renderPlot({
+     ggplot(plot_data(), aes(date, number), colour = "burlywood4")+
+               geom_col()+
+      ggtitle("Number of animals observed")+
+        scale_x_date(name = "Date",  date_breaks = "2 months", date_labels = "%m/%y", limits = c(input$date[1], input$date[2])) +
+        ylab("Number of individuals")+
+        cleanup +
+      theme(plot.title = element_text(family = "Helvetica", face = "bold", size = (15), colour = "blue4", hjust = 0.5),
+        axis.title = element_text(size = (15), colour = "blue4", face = "bold"))
+            
+  })
+  
+  # subset data for table 
+  table_data <- reactive({
      p %>%
-       filter(Type == input$type )
-   })
+        filter(
+           site_id == input$site) %>%
+        mutate(year = substr(date, 1,4))%>%
+        select("year", "number")%>%
+        group_by(year)%>%
+        summarise(max(number))
+  })
+  
+  
+  
+  
+  ## Create reactive table for data 
+  
+  output$survey_table <- renderDataTable({table_data()})
+     
+  
+  
+  }
+  
    
-   # select the site 
-   selected_site  = reactive ({
-     p %>%
-       filter(Type == input$site_id )
-   })
-   
-   # select the dat range for the output
-   
-   slected_date_range <- reactive ({req(input$daterangeInput)})
-   
-   # add map
-   output$my_map <- renderleaflet({
-     leaflet () %>%
-       addTiles() %>%
-       addMarkers(data = p, lng = `longitude, lat = ~latitude, layerID = ~`)
-   })
-   
-   
- }        
-          
-          
-str(p)          
-          
-          
- Canb <- leaflet () %>%
-   addTiles() %>%
-   fitBounds(lng1 = 149.0, lat1 = -35.0, lng2 = 150.0, lat2 = -36.0)%>%
-   addMarkers(data = p, ~longitude, ~latitude)
-
-Canb
+# 5 run the shiny APP   
 
 
 
+shinyApp(ui = ui, server = server)
+
+
+# establish and connect to shinyapps.io 
+
+install.packages('rsconnect')
+
+rsconnect::setAccountInfo(name='citizen-science-act-gov', token='6BAD85804410F294674F83FE6AD75B0C', secret='LWBMqwGfkVcSqOQvw1rSNLne6k3mH2dsQjrYMGpF')
 
 
 
-
-
+p %>%  filter(
+      site_id == "Cooma_Ck") %>%
+   mutate(year = substr(date, 1,4))%>%
+   group_by(year)%>%
+   summarise(max(number))
 
 
 
