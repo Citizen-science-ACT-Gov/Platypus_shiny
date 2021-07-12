65105d17df4e42cae32f32f1a1b9e569c018b666
Citizen-science-ACT-Gov <rodney.ubrihien@act.gov.au>
2021-07-12 13:37:57 +0000

    Update code with interactive map and plotly wrapping for figure

--- shiny_plat_2.R
+++ shiny_plat_2.R
@@ -25,6 +25,7 @@
 #install.packages("leaflet")
 
 #install.packages("plotly")
+# install.packages("DT")
 
 
 library(dplyr)
@@ -41,15 +42,21 @@ library(leaflet)
 
 library(plotly)
 
+library(DT)
+
 #2 Read and tidy platypus month data 
 
-p <- read.csv("survey_data.csv")
+platy <- read.csv("survey_data.csv")
 
 
-p <- p %>% mutate(
+platy <- platy%>% mutate(
   date = as.Date(date, format = "%d/%m/%Y"),
-  species = trimws(species)
+  species = trimws(species),
+  site_id = as.factor(site_id)
 )
+
+reaches <- platy %>% select("site_id", "latitude", "longitude") %>%
+  unique()
 
 
 cleanup <- theme(panel.grid.major = element_blank(),
@@ -84,12 +91,12 @@ ui <- fluidPage(tags$head(
   fluidRow(
     column(3, #select species 
       wellPanel(selectInput(inputId = "species", label = "Species",
-                  choices = unique(p$species),
+                  choices = unique(platy$species),
                   selected = "platypus"),
       
          # select site
       selectInput(inputId = "site", label = "Site",
-                  choices = unique(p$site_id),
+                  choices = unique(platy$site_id),
                   selected = "Cooma_Ck"),
       
       # select the time period for the output
@@ -98,7 +105,10 @@ ui <- fluidPage(tags$head(
                      format = "dd/mm/yyyy",
                      start = "2014-01-01",
                      separator = "to"))),
-      column(9, plotOutput(outputId = "col_plot", height = "400px"))),
+      column(9, plotlyOutput(outputId = "col_plot", height = "400px"),
+    "Figure 1. Number of sightings. Red dot indicates that a survey was conducted and bars indicate the 
+    number of individuals observed")
+    ),
   
   fluidRow(
       
@@ -106,50 +116,32 @@ ui <- fluidPage(tags$head(
       # Add leaflet map
       column(3,
              h2("Site map"),
-             "Click on Platypus icon to show site name",
+             "Click on Platypus icon to show site name and select site.",
              
-             leafletOutput("my_map")),
+             leafletOutput("mymap")),
     
-    
-      column(3,
+      # Add summary data table
+      column(2,
              h2("Data summary table"),
-             dataTableOutput(outputId = "survey_table")),
+             DT::dataTableOutput(outputId = "survey_table")),
       
-      column(4, 
-             tags$br(),
-             tags$br(),
+      column(5, 
+             
              h2("Details of the surveys"),
              tags$br(),
-             "The data presented is collected by volunteers during August of each year as part of the Platypus month program. 
-             During the surveys data are collected at 10 sites along a reach of the waterway. 
-             Reach length is generally ranges for 800 m to 1 km.  ", 
-             tags$br(),
-             tags$br(),
-             tags$br(), 
-             tags$br(),
-             tags$br()),
-      column(2, 
-      # add the Waterwatch logo to the bottom right hand corner of the map
-      tags$br(),
-      tags$br(),
-      tags$br(),
-      tags$br(), 
-      tags$br(),
-      tags$br(),
-      tags$br(),
-      tags$br(),
-      tags$br(), 
-      tags$br(),
-      tags$br(),
-      tags$br(), 
-      tags$br(),
-      tags$br(),
-      tags$img(height = 100, width = 250,
-               src = "https://f079a2602f82498e927c63b0219b50f9.app.rstudio.cloud/file_show?path=%2Fcloud%2Fproject%2FWaterwatch_logo_Upper_Murrumbidgee.png"),
-      
-      )
+             "Platypus month is conducted during August. 
+             The sites currently being surveyed are Cooma Creek, Jerrabomberra Creek, below Coppins Crossing, Mittagang Crossing, Point Hut Crossing, Scottsdale, Queanbeyan and Tidbinbilla. 
+             Data from some sites that have been previously included in the survey are also available. 
+             Currently, each year four surveys are conducted at each site to keep survey effort consistent across the sites. 
+             In previous years a different number of surveys were conducted at the different sites. At each site, 8 to 10 points are surveyed along a section of 800 m to 1 km. 
+             At each site surveys are conducted at dawn and dusk when Platypus are most active.", 
+            ),
+     
+     column(2, tags$img(height = 100, width = 250,
+               src = "https://f079a2602f82498e927c63b0219b50f9.app.rstudio.cloud/file_show?path=%2Fcloud%2Fproject%2FWaterwatch_logo_Upper_Murrumbidgee.png"))
+     ))
     
-  ))
+
 
 
 
@@ -163,19 +155,19 @@ html_legend <- "<img src='https://github.com/Citizen-science-ACT-Gov/platy_image
 
 
 
-server <- function (input, output) {
-  output$my_map <- renderLeaflet({
+server <- function (input, output, session) {
+  output$mymap <- renderLeaflet({
     
-    leaflet(p) %>%
+    leaflet(reaches) %>%
       addTiles() %>%
       fitBounds(lng1 = 148.8, lat1 = -35.1, lng2 = 149.35, lat2 = -36.3)%>%
       addProviderTiles("Esri.WorldImagery") %>%
-      addMarkers(~longitude, ~latitude, icon = platyIcons[p$Type], popup = (p$site_id), labelOptions = labelOptions(noHide = T, direction = "bottom",
-                                                                                                                    style = list(
-                                                                                                                      "color" = "black",
-                                                                                                                      "font-family" = "serif",
-                                                                                                                                                                                                                                        "font-size" = "12px"
-                                                                                                                    )))
+      addMarkers(data = reaches, ~longitude, ~latitude, layerId = ~site_id, 
+                  popup = ~site_id, icon = platyIcons[p$Type],
+                 labelOptions = labelOptions(noHide = T, direction = "bottom", 
+                                             style = list("color" = "black",
+                                                          "font-family" = "serif",
+                                                          "font-size" = "12px")))
                  })
   leafletOutput('mymap', height = 600)
   
@@ -190,20 +182,35 @@ server <- function (input, output) {
           date <= input$date[2])
   })
   
+ 
+  # update selected site based on map click
+  
+  observeEvent(input$mymap_marker_click, {
+    p <- input$mymap_marker_click
+    
+    #updateSelectInput(session, "site", selected = p$Siteid) 
+    updateSelectInput(session, "site", "Update my site", selected = p$id)
+  })
+  
+  
+  
+
+  
   # Create a reactive plot based on the inputs listed
   
-  output$col_plot <- renderPlot({
-    ggplot(plot_data(), )+
+  output$col_plot <- renderPlotly({
+    ggplotly(
+      ggplot(plot_data(), )+
       geom_col(aes(date, number, colour = "Animals observed"))+
       geom_point(aes(date, Survey, colour = "Survey conducted"))+
       ggtitle("Number of animals observed")+
-      scale_x_date(name = "Date",  date_breaks = "2 months", date_labels = "%m/%y", limits = c(input$date[1], input$date[2])) +
+      scale_x_date(name = "Date",  date_breaks = "6 months", date_labels = "%m/%y", limits = c(input$date[1], input$date[2])) +
       ylab("Number of individuals")+
       scale_colour_manual(values = c("black", "Red"))+
       labs(colour = "Legend")+
       cleanup +
       theme(plot.title = element_text(family = "Helvetica", face = "bold", size = (15), colour = "blue4", hjust = 0.5),
-            axis.title = element_text(size = (15), colour = "blue4", face = "bold"))
+            axis.title = element_text(size = (15), colour = "blue4", face = "bold")))
     
   })
   
@@ -212,14 +219,14 @@ server <- function (input, output) {
   
   # subset data for table 
   table_data <- reactive({
-    p %>%
+    platy %>%
       filter(
         species == input$species &
         site_id == input$site) %>%
       mutate(year = substr(date, 1,4))%>%
       select("year", "number")%>%
       group_by(year)%>%
-      summarise("Maximum number of animals detected" = max(number), "Average animals per survey" = mean(number), "Surveys conducted" = n())
+      summarise("Max count" = max(number), "Surveys conducted" = n())
   })
   
   
@@ -227,7 +234,11 @@ server <- function (input, output) {
   
   ## Create reactive table for data 
   
-  output$survey_table <- renderDataTable({table_data()})
+  output$survey_table <- DT::renderDataTable({DT::datatable(table_data (), options=list(iDisplayLength=5,                    # initial number of records
+                                                                                                       aLengthMenu=c(5,10),                  # records/page options
+                                                                                                       bLengthChange=0,                       # show/hide records per page dropdown
+                                                                                                       bFilter=0,                                    # global search box on/off
+                                                                                                       bInfo=0)) })
   
   
   
@@ -236,9 +247,10 @@ server <- function (input, output) {
 
 # 5 run the shiny APP   
 
-
-
 shinyApp(ui = ui, server = server)
+
+
+
 
 
 # establish and connect to shinyapps.io 
@@ -246,6 +258,7 @@ shinyApp(ui = ui, server = server)
 install.packages('rsconnect')
 
 rsconnect::setAccountInfo(name='citizen-science-act-gov', token='6BAD85804410F294674F83FE6AD75B0C', secret='LWBMqwGfkVcSqOQvw1rSNLne6k3mH2dsQjrYMGpF')
+
 
 
 
@@ -261,7 +274,7 @@ ggplot(p,  )+
   geom_col(aes(date, number, colour = "Green"))+
   geom_point(aes(date, Survey, colour = "Red"))+
   ggtitle("Number of animals observed")+
-  scale_x_date(name = "Date",  date_breaks = "2 months", date_labels = "%m/%y") +
+  scale_x_date(name = "Date",  date_breaks = "3 months", date_labels = "%m/%y") +
   ylab("Number of individuals")+
   labs(colour = "Legend")+
   scale_colour_manual(values = c("black", "Red"),
