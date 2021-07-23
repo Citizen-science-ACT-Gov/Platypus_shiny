5d8bbac634410ad380a28335abd4695397b78596
Citizen-science-ACT-Gov <rodney.ubrihien@act.gov.au>
2021-07-23 04:13:01 +0000

    update app to publis

--- connector.R
+++ connector.R
@@ -4,13 +4,13 @@
 
 install.packages('rsconnect')
 
-rsconnect::setAccountInfo(name='citizen-science-act-gov', 
-                          token='6BAD85804410F294674F83FE6AD75B0C', 
+rsconnect::setAccountInfo(name='citizen-science-act-gov',
+                          token='6BAD85804410F294674F83FE6AD75B0C',
                           secret='LWBMqwGfkVcSqOQvw1rSNLne6k3mH2dsQjrYMGpF')
 
 
 
 
 library(rsconnect)
-rsconnect::deployApp("App.R")
+rsconnect::deployApp()
 
